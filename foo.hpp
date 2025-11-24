8c7ace51d36e81c3a02e66d468de031892b85a6d
Mikołaj Urbański <mikiurb56@gmail.com>
2025-11-24 21:05:25 +0100

    .

--- foo.hpp
+++ foo.hpp
@@ -6,11 +6,21 @@
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
-{
-    std::vector< char > retval(people.size());
+std::vector< char > foo(std::list< Human >& people) {
+    
+   std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [&](Human& h) {
+        h.birthday();
+        if (h.isMonster() == true) {
+            retval.push_back('n');
+        }
+        else {
+            retval.push_back('y');
+        }
+    });
+
+    std::reverse(retval.begin(), retval.end());
 
     return retval;
 }
